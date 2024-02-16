4f1a2fef0f94cdc35bb760fb2b5968ea300f9858
Jonas Würzinger <jonaswurzinger@BernddastenBook.fritz.box>
2024-02-16 12:05:05 +0100

    updates to package names

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
-    install_requires=["sklearn", "scipy", "click>=6.0", "sklearn", "pyyaml"],
+    install_requires=["scikit-learn", "scikit-image", "scipy", "click>=6.0", "pyyaml", "matplotlib", "torch"],
     extras_require=extras_require,
     entry_points={"console_scripts": ["excursion=excursion.commandline:main"]},
     dependency_links=[],
